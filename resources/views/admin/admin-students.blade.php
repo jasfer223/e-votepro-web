5013826761a9eeaae69a231c744b41d16c139a21
jasper <jafernandez@nemsu.edu.ph>
2023-12-08 19:55:00 +0800

    Setup template

---
+++ resources/views/admin/admin-students.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.admin-app')
+@section('page-title', 'Admin Students')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Students</h1>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/layouts/partials/admin-header.blade.php
@@ -0,0 +1,14 @@
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+        <a href="index.html" class="logo d-flex align-items-center">
+            <img src="/NiceAdmin/assets/img/usg-logo.png" alt="">
+            <span class="d-none d-lg-block">E-VotePro</span>
+        </a>
+        <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+    @include('admin.layouts.partials.admin-navbar')
+
+</header><!-- End Header -->
